eaa35bcb145486008863ba916dff2de65db1cd99
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-02-11 18:36:38 -0300

    Create class Routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,4 @@
-import 'package:busca_peca/app/app_bindings.dart';
-import 'package:busca_peca/app/pages/home/home_page.dart';
-import 'package:busca_peca/app/pages/init/init_page.dart';
-import 'package:busca_peca/app/pages/login/login_page.dart';
-import 'package:busca_peca/app/pages/profile/profile_page.dart';
-import 'package:busca_peca/app/pages/register/register_page.dart';
+import 'package:busca_peca/app/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,33 +11,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       initialRoute: '/',
-      getPages: [
-        GetPage(
-          name: '/',
-          page: () => const InitPage(),
-          binding: InitBindings(),
-        ),
-        GetPage(
-          name: '/login',
-          page: () => const LoginPage(),
-          binding: LoginBindings(),
-        ),
-        GetPage(
-          name: '/register',
-          page: () => const RegisterPage(),
-          binding: RegisterBindings(),
-        ),
-        GetPage(
-          name: '/home',
-          page: () => const HomePage(),
-          binding: HomeBindings(),
-        ),
-        GetPage(
-          name: '/profile',
-          page: () => const ProfilePage(),
-          binding: ProfileBindings(),
-        ),
-      ],
+      getPages: Routes().getPages(),
     );
   }
 }
---
+++ lib/app/app_routes.dart
@@ -0,0 +1,43 @@
+import 'package:busca_peca/app/pages/init/init_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import 'app_bindings.dart';
+import 'pages/home/home_page.dart';
+import 'pages/login/login_page.dart';
+import 'pages/profile/profile_page.dart';
+import 'pages/register/register_page.dart';
+
+class Routes {
+
+  List<GetPage> getPages(){
+    return [
+        GetPage(
+          name: '/',
+          page: () => const InitPage(),
+          binding: InitBindings(),
+        ),
+        GetPage(
+          name: '/login',
+          page: () => const LoginPage(),
+          binding: LoginBindings(),
+        ),
+        GetPage(
+          name: '/register',
+          page: () => const RegisterPage(),
+          binding: RegisterBindings(),
+        ),
+        GetPage(
+          name: '/home',
+          page: () => const HomePage(),
+          binding: HomeBindings(),
+        ),
+        GetPage(
+          name: '/profile',
+          page: () => const ProfilePage(),
+          binding: ProfileBindings(),
+        ),
+      ];
+  }
+
+}
