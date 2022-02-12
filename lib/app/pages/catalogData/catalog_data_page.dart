dc52e88540c4bc81aa7b065299415e750127dbfe
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-02-12 13:30:07 -0300

    Create CatalogDataPage and CatalogDataController

---
+++ lib/app/pages/catalogData/catalog_data_page.dart
@@ -0,0 +1,14 @@
+import 'package:busca_peca/app/pages/catalogData/catalog_data_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CatalogDataPage extends GetView<CatalogDataController> {
+  const CatalogDataPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
